b70edab57384ac22a8f987be8bb4f2b9dcd6928b
lacsyy <olacomarkaidel@gmail.com>
2025-05-07 22:13:32 +0800

    Initial commit

---
+++ script.js
@@ -0,0 +1,17 @@
+const display = document.getElementById('display');
+
+function append(value) {
+    display.value += value;
+}
+
+function calculate() {
+    try {
+        display.value = eval(display.value);
+    } catch (e) {
+        display.value = 'Error';
+    }
+}
+
+function clearDisplay() {
+    display.value = '';
+}
